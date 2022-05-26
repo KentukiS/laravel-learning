bab215eafd18736cf663ec2911562c717270d80a
root <root@DESKTOP-88TCO1A.localdomain>
2022-05-27 00:16:20 +0300

    addit admin controller

---
+++ laravel/app/Http/Controllers/Blog/Admin/BaseController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Blog\Admin;
+
+use Illuminate\Http\Request;
+use APP\Htpp\Controller\BaseController as GuestBaseController;
+
+abstract class BaseController extends GuestBaseController
+{
+
+}
